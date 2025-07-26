dba2135101ee59c4e385e856a2bef10622589acf
FLYXTO <167959503+flyxto@users.noreply.github.com>
2025-07-27 01:35:14 +0530

    Update platform-adapter.ts

--- app/utils/platform-adapter.ts
+++ app/utils/platform-adapter.ts
@@ -1,4 +1,8 @@
+// Check for various serverless environments
 export const IS_CF_PAGES = typeof process === 'undefined';
+export const IS_VERCEL = typeof process !== 'undefined' && Boolean(process.env.VERCEL);
+export const IS_NETLIFY = typeof process !== 'undefined' && Boolean(process.env.NETLIFY);
+export const IS_SERVERLESS = IS_CF_PAGES || IS_VERCEL || IS_NETLIFY;
 
 // This hack is to prevent `node` modules/packages being bundled in the
 // Cloudflare Pages context, which causes an error.
