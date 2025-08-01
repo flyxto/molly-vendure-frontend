9cfd591a9d27eee5df90d96270e49dc73dfbe92b
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-08-01 14:37:50 +0700

    refactor: map the products to 3 columns instead of 4 columns for better visibility

--- app/components/products/FilterableProductGrid.tsx
+++ app/components/products/FilterableProductGrid.tsx
@@ -48,7 +48,7 @@ export function FilterableProductGrid({
       />
       {result.items.length > 0 ? (
         <div className=" w-full mt-8">
-          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-4 w-full">
+          <div className="grid grid-cols-2 gap-y-10 sm:grid-cols-3 gap-x-4 w-full">
             {result.items.map((item) => (
               <ProductCard key={item.productId} {...item} />
             ))}
