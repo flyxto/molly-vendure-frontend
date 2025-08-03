a728b5555536a941aaba6a721425bdcae6a58791
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-08-03 19:43:01 +0700

    style: change button button color and update styles according to the theme

--- app/routes/sign-up.success.tsx
+++ app/routes/sign-up.success.tsx
@@ -11,15 +11,15 @@ export default function SuccessPage() {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md border-2 rounded-md border-green-600">
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8 min-h-svh">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md border-2 rounded-md border-[#AF803C]">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <Form className="space-y-6" method="post">
             <div>
               <div className="flex justify-center">
                 <div className="flex-grow">
                   <CheckCircleIcon
-                    className="h-20 w-20 m-auto mb-2 text-green-600"
+                    className="h-20 w-20 m-auto mb-2 text-[#AF803C]"
                     aria-hidden="true"
                   />
                 </div>
@@ -27,7 +27,7 @@ export default function SuccessPage() {
               <p className="text-center mb-5">{t('account.createdMessage')}</p>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium bg-[#AF803C] text-white shadow-xs hover:bg-[#AF803C]/80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
               >
                 {t('account.goHome')}
               </button>
